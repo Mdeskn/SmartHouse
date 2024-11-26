b5dff7077bdb239895c32fe918cf83870ae7783c
zahra <mahdionzahra2002@gmail.com>
2024-11-26 21:03:39 +0100

    Update WindSource.java

--- src/smarthouse/models/WindSource.java
+++ src/smarthouse/models/WindSource.java
@@ -1,1 +1,13 @@
+package smarthouse.models;
+
+public class WindSource extends EnergySource {
+    public WindSource(String name, double energyGenerated, double regenerateRate) {
+        super(name, energyGenerated, regenerateRate);
+    }
+
+    @Override
+    public String getType() {
+        return "Wind";
+    }
+}
 
