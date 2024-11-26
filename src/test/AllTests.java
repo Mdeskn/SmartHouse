0e666ff73597d77f0720c3f808435a8ed6cdfd03
Mae <132174063+Mdeskn@users.noreply.github.com>
2024-11-26 21:04:09 +0100

    Update AllTests.java

--- src/test/AllTests.java
+++ src/test/AllTests.java
@@ -1,1 +1,19 @@
+package test;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({
+    ApplianceControllerTest.class,
+    EnergyControllerTest.class,
+    HouseManagerTest.class,
+    ExceptionHandlerTest.class,
+    FileHandlerTest.class,
+    LoggerTest.class,
+    ResourceLockTest.class,
+    TaskSchedulerTest.class
+})
+public class AllTests {
+}
 
