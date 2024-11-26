ae079afee9b9f9d4973cdecadec61686378a09c1
zahra <mahdionzahra2002@gmail.com>
2024-11-26 21:04:10 +0100

    Update SolarSource.java

--- src/smarthouse/models/SolarSource.java
+++ src/smarthouse/models/SolarSource.java
@@ -1,1 +1,13 @@
+package smarthouse.models;
+
+public class SolarSource extends EnergySource {
+    public SolarSource(String name, double energyGenerated, double regenerateRate) {
+        super(name, energyGenerated, regenerateRate);
+    }
+
+    @Override
+    public String getType() {
+        return "Solar";
+    }
+}
 
