c3ca80723f59b1f313c394ac3f5f146d7e1dae3f
Mae <132174063+Mdeskn@users.noreply.github.com>
2024-11-26 21:01:11 +0100

    Update ResourceLock.java

--- src/smarthouse/concurrency/ResourceLock.java
+++ src/smarthouse/concurrency/ResourceLock.java
@@ -1,1 +1,18 @@
+package smarthouse.concurrency;
+
+public class ResourceLock {
+    private boolean locked;
+
+    public boolean isLocked() {
+        return locked;
+    }
+
+    public void lock() {
+        locked = true;
+    }
+
+    public void unlock() {
+        locked = false;
+    }
+}
 
