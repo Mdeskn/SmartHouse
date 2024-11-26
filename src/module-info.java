f88be53a6537d54ffb7a667872d03cc70da1762c
Mae <132174063+Mdeskn@users.noreply.github.com>
2024-11-26 21:03:47 +0100

    Update module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -1,1 +1,21 @@
+module SmartHouse {
+    requires java.base;
+    requires org.junit.jupiter.api;
+    requires org.junit.platform.commons;
+    requires org.junit.platform.suite.api;
+
+    exports smarthouse.controllers;
+    exports smarthouse.concurrency;
+    exports smarthouse.utils;
+    exports smarthouse.models;
+    exports smarthouse.resources.logs;
+
+    opens smarthouse.controllers to org.junit.platform.commons;
+    opens smarthouse.concurrency to org.junit.platform.commons;
+    opens smarthouse.utils to org.junit.platform.commons;
+    opens smarthouse.models to org.junit.platform.commons;
+    opens smarthouse.resources.logs to org.junit.platform.commons;
+
+    opens test to org.junit.platform.commons;
+}
 
