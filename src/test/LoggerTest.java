80575f95107f63ada3497fdd905bf1317bb914d2
Mae <132174063+Mdeskn@users.noreply.github.com>
2024-11-26 21:06:16 +0100

    Update LoggerTest.java

--- src/test/LoggerTest.java
+++ src/test/LoggerTest.java
@@ -1,1 +1,22 @@
+package test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import smarthouse.resources.logs.Logger;
+
+class LoggerTest {
+    @Test
+    void testLogSimulation() {
+        Logger.logSimulation("Test Simulation Log");
+        String logs = Logger.readSimulationLogs();
+        Assertions.assertTrue(logs.contains("Test Simulation Log"));
+    }
+
+    @Test
+    void testClearLogs() {
+        Logger.clearSimulationLogs();
+        String logs = Logger.readSimulationLogs();
+        Assertions.assertTrue(logs.isEmpty());
+    }
+}
 
