b3871e1ceba8dc23e37f27b965f2a5e8c16b7ec0
zahra <mahdionzahra2002@gmail.com>
2024-11-26 21:03:10 +0100

    Update WaterSource.java

--- src/smarthouse/models/WaterSource.java
+++ src/smarthouse/models/WaterSource.java
@@ -1,1 +1,12 @@
+package smarthouse.models;
 
+public class WaterSource extends EnergySource {
+    public WaterSource(String name, double energyGenerated, double regenerateRate) {
+        super(name, energyGenerated, regenerateRate);
+    }
+
+    @Override
+    public String getType() {
+        return "Water";
+    }
+}
