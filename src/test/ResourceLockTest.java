6d7639fd7debada9fada147d797460b26b893ca0
Vida Bahrami <bahrami.vida68@gmail.com>
2024-11-26 21:18:43 +0100

    Update ResourceLockTest.java

--- src/test/ResourceLockTest.java
+++ src/test/ResourceLockTest.java
@@ -1,1 +1,19 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import smarthouse.concurrency.ResourceLock;
+
+class ResourceLockTest {
+    @Test
+    void testLockAndUnlock() {
+        ResourceLock lock = new ResourceLock();
+
+        lock.lock();
+        assertTrue(lock.isLocked(), "Lock should be locked");
+
+        lock.unlock();
+        assertFalse(lock.isLocked(), "Lock should be unlocked");
+    }
+}
 
