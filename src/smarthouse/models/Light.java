2d29f2febad355cce4eb791003b3b9a949823c2d
Vida Bahrami <bahrami.vida68@gmail.com>
2024-11-26 21:15:32 +0100

    Update Light.java

--- src/smarthouse/models/Light.java
+++ src/smarthouse/models/Light.java
@@ -1,1 +1,13 @@
 
+package smarthouse.models;
+
+public class Light extends Appliance {
+    public Light(String name, double energyConsumption) {
+        super(name, energyConsumption);
+    }
+
+    @Override
+    public String getType() {
+        return "Light";
+    }
+}
