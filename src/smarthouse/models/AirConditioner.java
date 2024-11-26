9bc91c6764159d7fedff361f52ce960db4ad2acb
Vida Bahrami <bahrami.vida68@gmail.com>
2024-11-26 21:14:21 +0100

    Update AirConditioner.java

--- src/smarthouse/models/AirConditioner.java
+++ src/smarthouse/models/AirConditioner.java
@@ -1,1 +1,13 @@
+package smarthouse.models;
+
+public class AirConditioner extends Appliance {
+    public AirConditioner(String name, double energyConsumption) {
+        super(name, energyConsumption);
+    }
+
+    @Override
+    public String getType() {
+        return "Air Conditioner";
+    }
+}
 
