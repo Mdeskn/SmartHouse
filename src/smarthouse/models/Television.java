f0ce483eefc45b1ceebdcf7152e4619aac2cc8bd
Vida Bahrami <bahrami.vida68@gmail.com>
2024-11-26 21:16:40 +0100

    Update Television.java

--- src/smarthouse/models/Television.java
+++ src/smarthouse/models/Television.java
@@ -1,1 +1,12 @@
+package smarthouse.models;
 
+public class Television extends Appliance {
+    public Television(String name, double energyConsumption) {
+        super(name, energyConsumption);
+    }
+
+    @Override
+    public String getType() {
+        return "Television";
+    }
+}
